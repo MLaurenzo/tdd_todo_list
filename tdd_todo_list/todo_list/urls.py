49d98107f26c0684babf271bbcb736c3b3e00050
Laurent BERTHOLLE <laurent@teemo.co>
2018-08-15 21:53:51 +0000

    Add rest api for Todo.

--- tdd_todo_list/todo_list/urls.py
+++ tdd_todo_list/todo_list/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^todos/$', views.todo_list),
+    url(r'^todos/(?P<pk>[0-9]+)/$', views.todo_detail),
 ]
--- tdd_todo_list/todo_list/views.py
+++ tdd_todo_list/todo_list/views.py
@@ -1,8 +1,69 @@
-from django.http import HttpResponse
+from django.forms import modelformset_factory
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from todo_list.models import Todo
+from todo_list.serializers import TodoSerializer
 
 
 def index(request):
     template = loader.get_template('todo_list/index.html')
     context = {}
-    return HttpResponse(template.render(context, request))
+
+    TodoFormSet = modelformset_factory(Todo, fields=('__all__'))
+    if request.method == 'POST':
+        formset = TodoFormSet(request.POST, request.FILES)
+        print(formset)
+        if formset.is_valid():
+            formset.save()
+            # do something.
+    else:
+        formset = TodoFormSet()
+    return render(request, 'todo_list/index.html', {'formset': formset})
+
+@csrf_exempt
+def todo_list(request):
+    """
+    List all code todos, or create a new todo.
+    """
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def todo_detail(request, pk):
+    """
+    Retrieve, update or delete a code todo.
+    """
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(todo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
