738c76e8787f969a8aa3db705b0180d29d5ca84c
Laurent BERTHOLLE <laurent@teemo.co>
2018-08-15 00:33:20 +0000

    Use template for index view.

--- tdd_todo_list/todo_list/views.py
+++ tdd_todo_list/todo_list/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Welcome on the todo list application !")
+    template = loader.get_template('todo_list/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
