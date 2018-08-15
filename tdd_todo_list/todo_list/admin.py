acd8cd70485db5b942e9f31ab3017afb8877cdde
Laurent BERTHOLLE <laurent@teemo.co>
2018-08-15 21:30:31 +0000

    Add TodoAdmin.

--- tdd_todo_list/todo_list/admin.py
+++ tdd_todo_list/todo_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_list.models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Todo, TodoAdmin)
